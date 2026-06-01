48267bca4305b7d24462a47edcf02d9635c1db73
Heitor <161908829+Heitor-M33n@users.noreply.github.com>
2026-06-01 14:14:26 -0300

    Revisão nos inserts

--- seed.sql
+++ seed.sql
@@ -5,31 +5,16 @@ INSERT INTO public.clientes (nome, email, cpf) VALUES
 	('Julia Costa', 'julia.c@email.com', '45678901234'),
 	('Fernanda Lima', 'fernanda.l@email.com', '56789012345');
 
-INSERT INTO public.produtos (nome_produto, preco) VALUES
-	('Café Espresso Tradicional', 8.00),
-	('Café Cappuccino Italiano', 12.00),
-	('Fatia de Bolo de Cenoura', 10.00),
-	('Pão de Queijo Recheado', 7.00),
-	('Café Coado na Mesa', 6.00);
+INSERT INTO public.produtos (produto, estoque, preco) VALUES
+	('Café Espresso Tradicional', 100, 8.00),
+	('Café Cappuccino Italiano', 70, 12.00),
+	('Fatia de Bolo de Cenoura', 20, 10.00),
+	('Pão de Queijo Recheado', 30, 7.00),
+	('Café Coado na Mesa', 60, 6.00);
 
-INSERT INTO public.estoque (id_produto, quantidade) VALUES
-	(1, 150), 
-	(2, 80),  
-	(3, 15),  
-	(4, 40),  
-	(5, 200);
-
-INSERT INTO public.pedidos (id_cliente, data_pedido) VALUES
-	(1, '2026-05-25'),
-	(2, '2026-05-26'),
-	(3, '2026-05-27'),
-	(4, '2026-05-28'),
-	(5, '2026-05-29');
-
-INSERT INTO public.produto_pedido (id_pedido, id_produto, quantidade) VALUES
-	(1, 1, 2), 
-	(1, 3, 1), 
-	(2, 2, 1), 
-	(3, 4, 3), 
-	(4, 5, 1), 
-	(5, 1, 1);
+INSERT INTO public.pedidos (id_cliente, id_produto) VALUES
+	(1, 1),
+	(2, 2),
+	(3, 3),
+	(4, 3),
+	(5, 4);
